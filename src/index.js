3cad5ef3414d7ce3de548ed5dd7f5902c7771a63
Liam Eller <liamthelastson@gmail.com>
2022-04-26 15:41:48 -0700

    Add about, portfolio and contact me sections wusing bootstrap 5 and custom styling

--- src/index.js
+++ src/index.js
@@ -2,3 +2,11 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import '@fortawesome/fontawesome-free';
+
+window.onscroll = () => {scrollFunction();};
+
+const scrollFunction = () => {
+  if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.backgroundColor = "white";
+  }
+};
