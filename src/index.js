39cb881b49599118dba8b47a253a47235ccdfb10
Liam Eller <liamthelastson@gmail.com>
2022-04-26 19:04:20 -0700

    Add progress bar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import '@fortawesome/fontawesome-free';
 
-window.onscroll = () => {scrollFunction();};
+const progressBar = $(".progress-bar");
 
-const scrollFunction = () => {
-  if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("navbar").style.backgroundColor = "white";
-  }
-};
+function scrollProgress() {
+  const scrollTop = document.documentElement.scrollTop;
+  const clientHeight = document.documentElement.clientHeight;
+  const scrollHeight = document.body.scrollHeight;
+  const scrollPercentage = (scrollTop / (scrollHeight - clientHeight)) * 100;
+  progressBar.css('--width', Math.round(scrollPercentage));
+  progressBar.attr("aria-valuenow", scrollPercentage);
+}
+
+document.addEventListener("scroll", scrollProgress);
+
+
+
+// window.onscroll = function() {scrollFunction();};
+
+// function scrollFunction() {
+//   if(document.body.scrollTop > 2 || document.documentElement.scrollTop > 2) {
+//     console.alert("if fired");
+//     document.getElementById("navbar").style.backgroundColor = "red";
+//   } else {
+//     console.alert("else fired");
+//     document.getElementById("navbar").style.backgroundColor = "blue";
+//   }
+// }
