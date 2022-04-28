4ecdcce71d2d1148aa07446157891052c44ed63d
Liam Eller <liamthelastson@gmail.com>
2022-04-27 18:44:15 -0700

    Add additional styling, github repos and images

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 function scrollProgress() {
-  const progressBar = $(".progress-bar");
   const scrollTop = document.documentElement.scrollTop;
   const clientHeight = document.documentElement.clientHeight;
   const scrollHeight = document.body.scrollHeight;
   const scrollPercentage = (scrollTop / (scrollHeight - clientHeight)) * 100;
-  progressBar.css('--width', Math.round(scrollPercentage));
-  progressBar.attr("aria-valuenow", scrollPercentage);
+
+  $(".progress-bar").css('--width', Math.round(scrollPercentage));
+  $(".progress-bar").attr("aria-valuenow", scrollPercentage);
 }
 
 $(document).on("scroll", scrollProgress);
